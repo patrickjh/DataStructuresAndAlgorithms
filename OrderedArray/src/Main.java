95d000bd2e8f72e532bf922dbd78922acab00170
Patrick Haggerty <patrick@lessonswithpatrick.com>
2016-07-04 19:44:46 -0700

    added binary search to insert method

--- OrderedArray/src/Main.java
+++ OrderedArray/src/Main.java
@@ -38,14 +38,53 @@ class OrdArray
 
 	public void insert(long value)
 	{
+		int lowerBound = 0;
+		int upperBound = nElems-1;
+		int curIn;
 		int j;
-		for(j=0; j<nElems; j++)
-			if(a[j] > value)
-				break;
+
+
+		/* Thinking. Need to have the binary search find the pair of indexes to insert into. 
+		That means the lower bound has to be just one below the upper bound. 
+		The lower bound needs to be less than the value to insert.
+		The upper bound needs to be greater
+		The upper index minus the lower index has to equal 1. 
+		On each iteration, they will look at the current index -
+		Current index is halfway between lower bound and upper bound
+		If this value is greater than the insertion value, we need to move the lower bound up
+		If it is less than the insertion value we need to move the upper bound down. 
+		We need to repeat this process until they are one apart, then insert above the lower bound
+		When we find this insertion point, we need to change the index of every 
+		higher entry in the array up one and put that index = to the value.
+
+		*/
+		if (nElems == 0) {
+			a[0] = value;
+			nElems++;
+			return;
+		}
+
+
+		while(1 < upperBound - lowerBound) {
+			curIn = (lowerBound + upperBound) / 2;
+			if (value > a[curIn])
+				lowerBound = curIn+1;
+			else
+				upperBound = curIn-1;
+		}
+		if (value > a[upperBound])
+			j = upperBound + 1;
+		else if (value < a[upperBound] && value > a [lowerBound])
+			j = upperBound;
+		else
+			j = lowerBound;
+
 		for(int k=nElems; k>j; k--)
 			a[k] = a[k-1];
 		a[j] = value;
 		nElems++;
+		System.out.println("Inserted " + value);
+
 
 	}
 
@@ -82,15 +121,25 @@ public class Main
 		arr = new OrdArray(maxSize);
 
 		arr.insert(77);
+		arr.display();
 		arr.insert(99);
+		arr.display();
 		arr.insert(44);
+		arr.display();
 		arr.insert(55);
+		arr.display();
 		arr.insert(22);
+		arr.display();
 		arr.insert(88);
+		arr.display();
 		arr.insert(11);
+		arr.display();
 		arr.insert(00);
+		arr.display();
 		arr.insert(66);
+		arr.display();
 		arr.insert(33);
+		arr.display();
 
 		int searchKey = 55;
 		if( arr.find(searchKey) != arr.size())
