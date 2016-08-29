a1d7d1a1b02b560c01f6bba3bd63ed4c401064e0
patrickjh <patrick@lessonswithpatrick.com>
2016-08-29 12:02:55 -0700

    added bubble sort

---
+++ Chapter3SimpleSorts/AllTests.java
@@ -0,0 +1,16 @@
+package dataStructuresAndAlgorithms.chapter3SimpleSorts;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Question_3_1Test.class, 
+				Question_3_2Test.class, 
+				Question_3_3Test.class, 
+				Question_3_4Test.class,
+				Question_3_5Test.class, 
+				Question_3_6Test.class })
+public class AllTests {
+
+}
---
+++ Chapter3SimpleSorts/Question_3_4Test.java
@@ -0,0 +1,14 @@
+package dataStructuresAndAlgorithms.chapter3SimpleSorts;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Question_3_4Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
