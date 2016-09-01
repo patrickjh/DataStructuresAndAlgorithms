f99654067ec4e7d045f82bf8e12a84b36e2115d0
Patrick Haggerty <patrick@lessonswithpatrick.com>
2016-09-01 15:07:13 -0700

    started stack class

---
+++ chapter4StacksAndQueues/StackApp.java
@@ -0,0 +1,13 @@
+class StackX {
+	private int maxSize;
+	private long[] stackArray;
+	private int top;
+	
+	public StackX(int max) {
+		maxSize = max;
+		stackArray = new long[maxSize];
+		top = -1;
+	}
+	
+	
+}
