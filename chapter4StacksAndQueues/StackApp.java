38b418984c89996fafc27c64daea3c25ecd87ba1
patrickjh <patrick@lessonswithpatrick.com>
2016-09-02 16:18:11 -0700

    finished stack class

--- chapter4StacksAndQueues/StackApp.java
+++ chapter4StacksAndQueues/StackApp.java
@@ -1,3 +1,5 @@
+package dataStructuresAndAlgorithms.chapter4StacksAndQueues;
+
 class StackX {
 	private int maxSize;
 	private long[] stackArray;
@@ -9,5 +11,43 @@ class StackX {
 		top = -1;
 	}
 	
+	public void push(long j) {
+		stackArray[++top] = j;
+	}
 	
+	public long pop() {
+		return stackArray[top--];
+	}
+	
+	public long peek() {
+		return stackArray[top];
+	}
+	
+	public boolean isEmpty() {
+		return (top == -1);
+	}
+	
+	public boolean isFull() {
+		return(top == maxSize-1);
+	}
+	
+}
+
+public class StackApp{
+	public static void main(String[] args) {
+		StackX theStack = new StackX(10);
+		theStack.push(20);
+		theStack.push(40);
+		theStack.push(60);
+		theStack.push(80);
+		
+		while( !theStack.isEmpty()) {
+			long value = theStack.pop();
+			System.out.print(value);
+			System.out.print(" ");
+			
+		}
+		
+		System.out.println("");
+	}
 }
